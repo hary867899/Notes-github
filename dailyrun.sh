21ca746981929fc35214bbc8358f79b03c573adb
hary867899 <mhari4tech@gmail.com>
2024-05-21 13:24:27 +0530

    first push of everything

---
+++ dailyrun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git add .
+date="20/05/2024"
+git commit -m "$date"
+
+git push origin main
---
+++ git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git remote remove origin
+commit='first push of everything'
+#reponame=''
+url="https://github.com/hary867899/Notes-github.git"
+git init
+git add .
+git commit -m "$commit"
+git branch -M main
+git remote add origin $url
+git push -u origin main
+
